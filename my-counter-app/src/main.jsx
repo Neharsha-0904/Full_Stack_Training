fad4a8457baa4e952e3a486be0a42bf7b6343401
Neharsha <kannegantivishnu0904@gmail.com>
2025-09-15 12:07:59 +0530

    d6p1

---
+++ my-counter-app/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Temp from './temp.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Temp />
+    
+  </StrictMode>,
+)
---
+++ my-counter-app/src/temp.jsx
@@ -0,0 +1,6 @@
+function temp(){
+    return(
+        <h2>hi this is vishnu</h2>
+    );
+}
+export default temp
